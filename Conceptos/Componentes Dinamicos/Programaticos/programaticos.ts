5ab1b30c9511a5275d0f7864fa65265cb23873a0
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-03-23 19:43:55 -0300

    terminado por hoy

---
+++ Conceptos/Componentes Dinamicos/Programaticos/programaticos.ts
@@ -0,0 +1,25 @@
+
+
+constructor(
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
+
+
+private showErrorAlert(message: string) {
+    // const alertCmp = new AlertComponent();
+    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(
+      AlertComponent
+    );
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
+}
+
+shared/placeholder/directive
---
+++ Conceptos/Modulos/patron-recomendado.ts
@@ -0,0 +1,5 @@
+1/*  
+modulo por caracteristicas
+1 shared module para las directivas y cosas en comun
+1 core para los servicios 
+*/
