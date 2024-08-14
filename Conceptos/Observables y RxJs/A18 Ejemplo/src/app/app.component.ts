f3d96a8da49ac726297cc99d7707316e3f0c6c82
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-08-14 12:19:41 -0300

    signal to obs

--- Conceptos/Observables y RxJs/A18 Ejemplo/src/app/app.component.ts
+++ Conceptos/Observables y RxJs/A18 Ejemplo/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, OnInit, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private destroyRef: DestroyRef) {
+    // effect(() => {
+    //   console.log(`clicked button ${this.clickCount()} times`);
+    // });
+    // toObservable(this.clickCount)
+  }
 
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+
+  ngOnInit(): void {
+    const subscription = interval(1000)
+      .pipe(map((val) => val * 2))
+      .subscribe({
+        next: (value) => {
+          console.log(value);
+        },
+        // complete: () => {
+        //   console.log('complete');
+        // },
+        // error: (err) => {
+        //   console.log(err);
+        // }
+      });
+
+    const subscription2 = this.clickCount$.subscribe({
+      next: (value) => {
+        console.log(`clicked button ${this.clickCount()} times`);
+      },
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+      subscription2.unsubscribe();
+    });
+  }
+
+  onClick() {
+    this.clickCount.update((prevCount) => prevCount + 1);
+  }
 }
