6d01cc3c4b3f1bfc56702792681516b6d58fbe85
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-08-14 11:31:39 -0300

    async pipe

--- Conceptos/Deteccion de cambios/A18 Ejemplo/src/app/messages/messages-list/messages-list.component.ts
+++ Conceptos/Deteccion de cambios/A18 Ejemplo/src/app/messages/messages-list/messages-list.component.ts
@@ -1,39 +1,18 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  DestroyRef,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent implements OnInit {
-  constructor(
-    private messagesService: MessagesService,
-    private cdRef: ChangeDetectorRef,
-    private destroyRef: DestroyRef
-  ) {}
-
-  messages: string[] = [];
-
-  ngOnInit(): void {
-    const subscription = this.messagesService.messages$.subscribe(
-      (messages) => {
-        this.messages = messages;
-        this.cdRef.markForCheck();
-      }
-    );
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
+export class MessagesListComponent {
+  constructor(private messagesService: MessagesService) {}
+  messages$ = this.messagesService.messages$;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
