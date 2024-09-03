37be264a2cc3c24450caf1edfbb0321b9273299d
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-03 11:06:20 -0300

    reactive form example added

--- Conceptos/A18 - Forms/Reactive Driven  Forms/src/app/auth/login/login.component.ts
+++ Conceptos/A18 - Forms/Reactive Driven  Forms/src/app/auth/login/login.component.ts
@@ -1,9 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { debounceTime, of } from 'rxjs';
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+  return { doesNotContainQuestionMark: true };
+}
+
+//i.e here you would send a be req to check uniqueness
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'admin@example.com') {
+    return of(null);
+  }
+  return of({ notUniqueEmail: true });
+}
+
+//this only works client side 
+let initialEmailValue = ''
+const savedForm = window.localStorage.getItem('saved-login-form');
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm);
+  initialEmailValue = loadedForm.email
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  constructor(private destroyRef: DestroyRef) {}
+
+  ngOnInit(): void {
+    // const savedForm = window.localStorage.getItem('saved-login-form');
+    // if (savedForm) {
+    //   const loadedForm = JSON.parse(savedForm);
+    //   this.loginForm.patchValue({ email: loadedForm.email });
+    // }
+    const subscription = this.loginForm.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => {
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          );
+        },
+      });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  loginForm = new FormGroup({
+    email: new FormControl(initialEmailValue, {
+      validators: [Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique],
+    }),
+    password: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        mustContainQuestionMark,
+      ],
+    }),
+  });
+
+  onSubmit() {
+    const enteredEmail = this.loginForm.value.email;
+    const enteredPassword = this.loginForm.value.password;
+
+    console.log(this.loginForm);
+  }
+
+  get emailIsInvalid() {
+    return (
+      this.loginForm.controls.email.touched &&
+      this.loginForm.controls.email.dirty &&
+      this.loginForm.controls.email.invalid
+    );
+  }
+
+  get passwordIsInvalid() {
+    return (
+      this.loginForm.controls.password.touched &&
+      this.loginForm.controls.password.dirty &&
+      this.loginForm.controls.password.invalid
+    );
+  }
+}
